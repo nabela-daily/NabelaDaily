76ef1559459c2d66c889e4279a6855cb200c5ec4
YOAN <yoannikaros29@gmail.com>
2022-06-10 00:10:19 +0700

    Post

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Livewire\Posts;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +26,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/post', Posts::class);
